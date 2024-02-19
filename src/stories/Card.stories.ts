d0d9bae7337b8d8468358431b9c5b979356b98ef
Sai Kumar N <sai98nsk@gmail.com>
2024-02-19 11:32:20 -0500

    feat: added stories for button and select

---
+++ src/stories/Card.stories.ts
@@ -0,0 +1,33 @@
+import Card from "../components/Card/Card";
+import type { Meta, StoryObj } from "@storybook/react";
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Components/Card",
+  component: Card,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  //   argTypes: {
+  //     backgroundColor: { control: "color" },
+
+  //     title
+  //     description,
+  //     imageUrl,
+  //     className,
+  //   },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Primary: Story = {
+  args: {
+    title: "Card Title",
+    description: "Card Description",
+    className: "bg-gray-100",
+    imageUrl: "https://images.unsplash.com/photo-1634304445213-3e3a6f3c1d7a",
+  },
+};
