0c7c48fa470fd76f10d56c3b6ee172fdf7fb4469
Sai Kumar N <sai98nsk@gmail.com>
2024-02-20 19:38:48 -0500

    fix: added theme colors to the project

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
@@ -8,9 +9,9 @@ const Button: React.FC<ButtonProps> = ({ onClick, children, variant }) => {
   let buttonClasses = "px-4 py-2 rounded focus:outline-none";
 
   if (variant === "primary") {
-    buttonClasses += " bg-blue-500 text-white";
+    buttonClasses += " text-white ";
   } else if (variant === "secondary") {
-    buttonClasses += " bg-gray-500 text-white";
+    buttonClasses += " accent-primary text-white";
   }
 
   return (
