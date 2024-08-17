f205ecaf6c350ba06022165fe74ffef3ac623472
Sai <sai98nsk@gmail.com>
2024-08-17 17:00:55 -0400

    feat: added plop js integration

--- plop.js
+++ plop.js
@@ -1,20 +1,62 @@
-export default function (plop) {
-  // component generator
-  plop.setGenerator("controller", {
-    description: "application controller logic",
+module.exports = function (plop) {
+  plop.setGenerator("action-based", {
+    description: "Generate files based on selected action",
     prompts: [
+      {
+        type: "list",
+        name: "action",
+        message: "What type of action do you want to take?",
+        choices: ["component", "service", "pages"],
+      },
       {
         type: "input",
         name: "name",
-        message: "Enter the component name",
+        message: "Enter a name:",
       },
     ],
-    actions: [
-      {
-        type: "add",
-        path: "src/{{name}}.js",
-        templateFile: "plop-templates/controller.hbs",
-      },
-    ],
+    actions: function (data) {
+      const actions = [];
+
+      switch (data.action) {
+        case "component":
+          actions.push({
+            type: "add",
+            path: "src/components/{{properCase name}}/{{properCase name}}.tsx",
+            templateFile: "src/plop-templates/component/component.hbs",
+          });
+          actions.push({
+            type: "add",
+            path: "src/components/{{properCase name}}/{{properCase name}}.test.tsx",
+            templateFile: "src/plop-templates/component/component.test.hbs",
+          });
+          actions.push({
+            type: "add",
+            path: "src/stories/{{properCase name}}.stories.ts",
+            templateFile: "src/plop-templates/component/component.stories.hbs",
+          });
+          break;
+        case "service":
+          actions.push({
+            type: "add",
+            path: "src/service/{{camelCase name}}/{{camelCase name}}Service.ts",
+            templateFile: "src/plop-templates/service/service.hbs",
+          });
+          // actions.push({
+          //   type: 'modify',
+          //   path: 'src/utils/types/index.d.ts',
+          //   templateFile: 'src/plop-templates/service/service.type.hbs',
+          // });
+          break;
+        case "pages":
+          actions.push({
+            type: "add",
+            path: "src/pages/{{camelCase name}}/index.tsx",
+            templateFile: "src/plop-templates/pages.hbs",
+          });
+          break;
+      }
+
+      return actions;
+    },
   });
-}
+};
